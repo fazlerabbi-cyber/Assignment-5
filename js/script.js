f4c0917dac44631ff02e048510ae67394bbd4a7c
fazlerabbi-cyber <fazle6423@gmail.com>
2024-09-23 20:58:38 +0600

    added file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function getInputValueById(id) {
-    return parseFloat(document.getElementById(id).value)
+    const inputValue = parseFloat(document.getElementById(id).value)
+    return inputValue
 }
 
 const donateBtn = document.getElementById('donateBtn1')
@@ -8,8 +9,10 @@ donateBtn.addEventListener('click', function () {
     
    
     const inputOne = getInputValueById('inputOne')
-    
-    document.getElementById('addMoney').innerText = inputOne
+    const inputFastValue = document.getElementById('addMoney').innerText
+    const addMoney = parseFloat(inputFastValue)
+     const noakhaliMoney = inputOne + addMoney;
+    document.getElementById('addMoney').innerText= noakhaliMoney
 
 
     document.getElementById('donateOne').innerText = inputOne
@@ -28,49 +31,58 @@ donateBtn.addEventListener('click', function () {
     
 })
 
-
+// 2nd card 
 const donateBtn2 = document.getElementById('donateBtn2')
 donateBtn2.addEventListener('click', function() {
 
-    const inputTwo = getInputValueById('input2')
-    document.getElementById('addMoney2').innerText = inputTwo
+    const input2 = getInputValueById('input2')
+    const inputFastValue = document.getElementById('addMoney2').innerText
+    const addMoney2 = parseFloat(inputFastValue)
+     const noakhaliMoney = input2 + addMoney2;
+    document.getElementById('addMoney2').innerText= noakhaliMoney
 
-    document.getElementById('donateTwo').innerText = inputTwo
-
-
-
+    document.getElementById('donateTwo').innerText = inputOne
     const navMoney = parseFloat(document.getElementById('navMoney').innerText)
+ 
 
-    const result = navMoney - inputTwo
+    const result = navMoney - input2
     document.getElementById('navMoney').innerText = result
 
-    if(inputTwo <= 0 || isNaN(inputTwo)) {
+
+
+    if(inputTwo <= 0 || isNaN(input2)) {
         alert('Valid Donate Amount')
     }
+
     
 })
 
-
-
+// 3rd 
 const donateBtn3 = document.getElementById('donateBtn3')
 donateBtn3.addEventListener('click', function() {
-    const inputThree = getInputValueById('input3')
-    document.getElementById('addMoney3').innerText = inputThree
 
-    document.getElementById('donateThree').innerText = inputThree
+    const input3 = getInputValueById('input3')
+    const inputFastValue = document.getElementById('addMoney3').innerText
+    const addMoney3 = parseFloat(inputFastValue)
+     const noakhaliMoney = input3 + addMoney3;
+    document.getElementById('addMoney3').innerText= noakhaliMoney
 
+    document.getElementById('donateThree').innerText = input3
     const navMoney = parseFloat(document.getElementById('navMoney').innerText)
+ 
 
-    const result = navMoney - inputThree
+    const result = navMoney - input3
     document.getElementById('navMoney').innerText = result
 
 
-    if(inputThree <= 0 || isNaN(inputThree)) {
+
+    if(inputThree <= 0 || isNaN(input3)) {
         alert('Valid Donate Amount')
     }
-    
+
     
 })
+
 
 
 // history btn btn-primary
@@ -84,7 +96,7 @@ history.addEventListener('click', function() {
 
  donate.classList.remove("btn", "btn-primary")
 
-document.getElementById('main-section').classList.add('hidden')
+document.getElementById('div-container').classList.add('hidden')
 document.getElementById('history-section').classList.remove('hidden')
 
 })
@@ -94,6 +106,9 @@ document.getElementById('history-section').classList.remove('hidden')
     donate.classList.add("btn", "btn-primary")
 
     history.classList.remove("btn", "btn-normal")
+
+    document.getElementById('div-container').classList.remove('hidden')
+document.getElementById('history-section').classList.add('hidden')
 
    
   })
