d82844f842ba6229a62ea98827c798dece236e74
fazle <fazle6423@gmail.com>
2024-09-23 07:51:02 +0600

    added new file

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ donateBtn.addEventListener('click', function () {
 
     const result = navMoney - inputOne
     document.getElementById('navMoney').innerText = result
+
+
+
+    if(inputOne <= 0 || isNaN(inputOne)) {
+        alert('Valid Donate Amount')
+    }
     
     
 })
@@ -32,6 +38,11 @@ donateBtn2.addEventListener('click', function() {
 
     const result = navMoney - inputTwo
     document.getElementById('navMoney').innerText = result
+
+    if(inputTwo <= 0 || isNaN(inputOne)) {
+        alert('Valid Donate Amount')
+    }
+    
 })
 
 
@@ -46,5 +57,41 @@ donateBtn3.addEventListener('click', function() {
     const result = navMoney - inputThree
     document.getElementById('navMoney').innerText = result
 
+
+    if(inputThree <= 0 || isNaN(inputOne)) {
+        alert('Valid Donate Amount')
+    }
     
+    
+})
+
+
+// history btn btn-primary
+
+const history = document.getElementById('historyBtn')
+const  donate = document.getElementById('donateBtn')
+
+history.addEventListener('click', function() {
+    history.classList.add("btn", "btn-primary")  
+    donate.classList.remove("btn", "btn-normal")
+
+    document.getElementById('main-section').classList.add('hidden')
+
+
+    document.getElementById('historyBtn').classList.remove('hidden')
+
+})
+
+
+// donate 
+
+donate.addEventListener('click', function () {
+    history.classList.add("btn", "btn-normal")
+
+    donate.classList.remove("btn", "btn-primary")
+
+    document.getElementById('main-section').classList.remove('hidden')
+
+
+    document.getElementById('historyBtn').classList.add('hidden')
 })
